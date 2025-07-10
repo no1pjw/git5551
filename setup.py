5de508641f78e854b1b9c952a52525a68817c70c
Jeongwon Park <105582726+no1pjw@users.noreply.github.com>
2025-07-10 22:50:49 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+  name='shell',
+  version='0.0.1',
+  description='hihi',
+  url='https://github.com/no1pjw/git5551.git',
+  author='pjw',
+  author_email='asfd',
+  license='pjw',
+  packages=['hi'],
+  zip_safe=False,
+  install_requires=[
+    'flask'
+  ]
+)
